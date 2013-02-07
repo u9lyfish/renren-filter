eb5ff8771f2a4a358d3206f15da3da8b85b8965b
Yuyang Benjamin Liu <l.yuyang@gmail.com>
2013-02-07 17:27:49 +0800

    the js file in initial commit is for Renren-inspector, this commit replaces it

--- Renren-Filter/background.js
+++ Renren-Filter/background.js
@@ -27,10 +27,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 		article_num = articles.length; \
 		for (var i = articles.length - 1; i >= 0; i--) { \
 			var stype = articles[i].getElementsByTagName('figure')[0].getAttribute('data-stype'); \
-			if (!articles[i].getElementsByTagName('note')[0]) { \
-				var note = document.createElement('note'); \
-				note.innerHTML = stype; \
-				articles[i].appendChild(note); \
+			if (stype == 102 || stype == 103 || stype == 104 || stype == 107 || stype == 110 || \
+				stype == 1902 || stype == 1906 || stype == 1909 || \
+				stype == 2003 || stype == 2004 || stype == 2005 || stype == 2006 || stype == 2035 || \
+				stype == 3801 || stype == 3802 || \
+				(stype == 502 &&  \
+					articles[i].getElementsByTagName('div')[0].getAttribute('class') == 'content' && \
+					articles[i].getElementsByTagName('div')[1].getAttribute('class') == 'source')) { \
+				articles[i].style.cssText = 'border: 1px solid red'; \
 			}; \
 		}; \
 		\
