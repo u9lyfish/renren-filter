757da405be118b1dc68ff6d93e5efad1cb869ee8
Yuyang Benjamin Liu <l.yuyang@gmail.com>
2013-02-08 21:48:46 +0800

    now the script automatically downloads the latest list from the gist each time.

--- Renren-Inspector/contentscript.js
+++ Renren-Inspector/contentscript.js
@@ -1,14 +1,61 @@
+// all known 'data-stype' values
+var known_stype;
 
-window.onload = detectChange();
+// number of article elements in last check. this is used to decide (more efficiently)
+// whether page content has changed since last check
+var article_num = 0;
+
+// time (in milliseconds) between two rounds of check
+var interval = 500;
+
+// fetch latest gist content, update known_stype and start detecting webpage content
+fetchStypeList(updateStypeList);
+
+/**
+ * Performs an XMLHttpRequest to GitHub's API to get latest list of data-stype values.
+ */
+function fetchStypeList(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(data) {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText);
+        callback(data);
+      } else {
+        callback(null);
+      }
+    }
+  }
+  // Note that any URL fetched here must be matched by a permission in
+  // the manifest.json file!
+  var url = 'https://api.github.com/gists/4730574';
+  xhr.open('GET', url, true);
+  xhr.send();
+};
+
+/**
+ * Update array known_stype and call detectChange().
+ */
+function updateStypeList(response) {
+	if (response == null) {
+		return;
+	};
+
+	var content = response["files"]["renren-feed-data_stype-relation"]["content"];
+	known_stype = content.split(/[^0-9]+/);		// use non-numeric characters as delimiters
+	window.onload = detectChange();
+};
+
+/**
+ * 
+ */
 function detectChange() { 
-	var interval = 500;
 	var newsfeed = document.getElementById('newsfeed-module-box');
 	if (!newsfeed) {
 		setTimeout('detectChange()', interval);
 		return;
 	};
 
-	var article_num = 0;
 	var articles = newsfeed.getElementsByTagName('article');
 	if (!articles || article_num == articles.length) {
 		setTimeout('detectChange()', interval);
@@ -16,28 +63,10 @@ function detectChange() {
 	};
 
 	article_num = articles.length;
-	var known_stype = 
-	[
-	102, 103, 104, 107, 110, 
-	210, 
-	501, 502, 
-	601, 602, 
-	701, 702, 708, 709, 
-	801, 807, 
-	1101, 1804, 1902, 1903, 1904, 1906, 1907, 1909, 
-	2002, 2003, 2004, 2005, 2006, 2008, 2012, 2013, 2032, 2035, 2036, 
-	2401, 
-	3707, 3732, 3733, 
-	3801, 3802, 3803, 
-	8182, 8185, 8186, 
-	8301, 8302, 
-	9802,
-	9999 
-	];
 
 	for (var i = articles.length - 1; i >= 0; i--) {
 		var stype = articles[i].getElementsByTagName('figure')[0].getAttribute('data-stype');
-		if (known_stype.indexOf(parseInt(stype)) < 0) {
+		if (known_stype.indexOf(stype) < 0) {
 			articles[i].style.cssText = 'border: 1px solid red';
 		};
 		if (articles[i].getElementsByTagName('note').length == 0) {
