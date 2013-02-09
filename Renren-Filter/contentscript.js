f7f8f401d1b20a792a19f38608d501af4a663803
Yuyang Benjamin Liu <l.yuyang@gmail.com>
2013-02-09 11:12:58 +0800

    working version using mock data, instead of local storage.

--- Renren-Filter/contentscript.js
+++ Renren-Filter/contentscript.js
@@ -1,32 +1,57 @@
-	window.onload = detectChange();
-	function detectChange() { 
-		var interval = 500;
-		var newsfeed = document.getElementById('newsfeed-module-box');
-		if (!newsfeed) {
-			setTimeout('detectChange()', interval);
-			return;
-		};
-		
-		var article_num = 0;
-		var articles = newsfeed.getElementsByTagName('article');
-		if (!articles || article_num == articles.length) {
-			setTimeout('detectChange()', interval);
-			return;
-		};
-		
-		article_num = articles.length;
-		for (var i = articles.length - 1; i >= 0; i--) {
-			var stype = articles[i].getElementsByTagName('figure')[0].getAttribute('data-stype');
-			if (stype == 102 || stype == 103 || stype == 104 || stype == 107 || stype == 110 ||
-				stype == 1902 || stype == 1906 || stype == 1909 ||
-				stype == 2003 || stype == 2004 || stype == 2005 || stype == 2006 || stype == 2035 ||
-				stype == 3801 || stype == 3802 ||
-				(stype == 502 && 
-					articles[i].getElementsByTagName('div')[0].getAttribute('class') == 'content' &&
-					articles[i].getElementsByTagName('div')[1].getAttribute('class') == 'source')) {
-				articles[i].parentNode.removeChild(articles[i]);
-			};
-		};
-		
-		setTimeout('detectChange()', interval);
-	}; 
+// all known 'data-stype' values
+var banned_stype;
+
+// number of article elements in last check. this is used to decide (more efficiently)
+// whether page content has changed since last check
+var article_num = 0;
+
+// time (in milliseconds) between two rounds of check
+var interval = 500;
+
+
+loadConfig();
+window.onload = detectChange();
+
+
+/**
+ * Load configuration from local storage
+ */
+function loadConfig() {
+	banned_stype = ['102', '110'];
+};
+
+/**
+ * 
+ */
+ function detectChange() { 
+ 	if (!banned_stype) {
+ 		return;
+ 	};
+
+ 	var newsfeed = document.getElementById('newsfeed-module-box');
+ 	if (!newsfeed) {
+ 		setTimeout('detectChange()', interval);
+ 		return;
+ 	};
+
+ 	var articles = newsfeed.getElementsByTagName('article');
+ 	if (!articles || article_num == articles.length) {
+ 		setTimeout('detectChange()', interval);
+ 		return;
+ 	};
+
+ 	article_num = articles.length;
+
+ 	for (var i = articles.length - 1; i >= 0; i--) {
+ 		var stype = articles[i].getElementsByTagName('figure')[0].getAttribute('data-stype');
+ 		if (banned_stype.indexOf(stype) >= 0 || (stype == 502 && 
+ 			articles[i].getElementsByTagName('div')[0].class == 'content' &&
+ 			articles[i].getElementsByTagName('div')[1].class == 'source')) 
+ 		{
+ 			articles[i].style.cssText = 'border: 1px solid red';
+ 		};
+ 	};
+
+ 	setTimeout('detectChange()', interval);
+ };
+
